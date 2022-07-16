e9f99caa0fe119cb3f1ddfce8b6ea9984e556ea6
EMMANUEL <dammylola299@gmail.com>
2022-07-16 16:37:17 +0100

    trying to add chats

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { 
+  getFirestore, collection
+ } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBl17u7Ro-0W-ikrlL98U9W3fUyuIGJbNM",
@@ -16,3 +18,54 @@ initializeApp(firebaseConfig);
 // init services
 const db = getFirestore();
 
+// init collection reference
+const colRef = collection(db, 'chats');
+
+// auliualiua
+
+
+
+// CLASS SCRIPT
+class Chatroom {
+  constructor(room, username){
+    this.room = room;
+    this.username = username;
+    this.chats = colRef;
+  }
+  async addChat(message){
+    // format of a chat object
+    const now = new Date();
+    const chat = {
+      message: message,
+      username: this.username,
+      room: this.room
+      // created_At: 
+    };
+    // saving the chat document
+    const response =  this.chats.add(chat);
+    return response;
+  }
+}
+
+
+
+
+const chatroom = new Chatroom ('gaming', 'Da vinci');
+
+chatroom.addChat('hello everyone')
+  .then(() => {
+    console.log('chat added');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  })
+
+
+
+// Adding new chat documents to the chat collection
+
+// Setting up a real-time listener to get new chats
+
+// Updating the username
+
+// Updating the room

df7830d2cf019a120fb41e96d5278399f970ce71
EMMANUEL <dammylola299@gmail.com>
2022-07-30 16:34:04 +0100

    Updated username to the UI

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import { ChatUI } from "./ui";
 // DOM QUERIES
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
 
 // ADDING NEW CHATS TO BE DISPLAYED IN THE UI
 newChatForm.addEventListener('submit', e => {
@@ -18,6 +20,20 @@ newChatForm.addEventListener('submit', e => {
     .catch((err) => {
       console.log(err.message);
     })
+
+});
+
+// UPDATING USERNAME TO BE DISPLAYED IN THE UI
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // update name via the chatroom class
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  // reset the form
+  newNameForm.reset();
+  // show then hide the updated name as a mssg for a few secs...
+  updateMssg.innerText = `Your name was updated to ${newName}`;
 
 });
 
